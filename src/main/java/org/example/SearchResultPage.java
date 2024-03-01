f69dc0938d2ae5dc00b46e185e28463a8e8424e4
Dmitriy <97601613+kudmi1@users.noreply.github.com>
2024-03-01 21:30:31 +0200

    very last commit

--- src/main/java/org/example/SearchResultPage.java
+++ src/main/java/org/example/SearchResultPage.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.example.Book;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -40,14 +41,31 @@ public class SearchResultPage {
   public ArrayList<Book> extractBooks() {
     ArrayList<Book> listOfBooks = new ArrayList<>();
     for (WebElement element : searchResults) {
-      String title = wait.until(ExpectedConditions.visibilityOf(element.findElement(By.cssSelector("h2.a-size-mini")))).getText();
-      String authorText = wait.until(ExpectedConditions.visibilityOf(element.findElement(By.cssSelector(".a-row.a-size-base.a-color-secondary")))).getText();
+      String title = wait.until(ExpectedConditions.visibilityOf(element.findElement(By.cssSelector("h2.a-size-mini"))))
+          .getText();
+      String authorText = wait.until(
+          ExpectedConditions.visibilityOf(element.findElement(By.cssSelector(".a-row.a-size-base.a-color-secondary"))))
+          .getText();
       authorText = formatAuthorText(authorText);
 
       boolean isBestseller = !element.findElements(By.xpath(".//span[@class='a-badge-text']")).isEmpty();
 
-      String priceWhole = element.findElement(By.xpath(".//span[@class='a-price-whole']")).getText();
-      String priceFraction = element.findElement(By.xpath(".//span[@class='a-price-fraction']")).getText();
+      String priceWhole = "";
+      String priceFraction = "";
+
+      try {
+        WebElement priceWholeElement = element.findElement(By.xpath(".//span[@class='a-price-whole']"));
+        priceWhole = priceWholeElement.getText();
+      } catch (NoSuchElementException e) {
+        // Handle the case when priceWhole element is not found
+      }
+
+      try {
+        WebElement priceFractionElement = element.findElement(By.xpath(".//span[@class='a-price-fraction']"));
+        priceFraction = priceFractionElement.getText();
+      } catch (NoSuchElementException e) {
+        // Handle the case when priceFraction element is not found
+      }
       String price = priceWhole + "." + priceFraction;
 
       listOfBooks.add(new Book(title, authorText, price, isBestseller));
